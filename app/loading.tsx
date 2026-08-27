58c57c5ee7feade97ab582191d74f221970cf717
Pradeep Chandu <58159402+oreodigi@users.noreply.github.com>
2026-08-27 17:41:15 +0530

    Improve branded loading screen

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,8 @@
 import { BrandIcon } from "@/components/BrandLogo";
 
 export default function Loading() {
-  return <div className="app-loader"><div className="loader-icon"><BrandIcon size={86}/></div><div className="loader-line"><span/></div></div>;
+  return <div className="app-loader">
+    <div className="loader-brand"><BrandIcon size={170}/><strong>Sri Gaur Nitai</strong><small>Spirituality • Creativity • Community</small></div>
+    <div className="loader-line"><span/></div>
+  </div>;
 }
