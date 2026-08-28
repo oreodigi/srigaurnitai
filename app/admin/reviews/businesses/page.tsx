b8bdac8d9902e20f69033f1762f0e7bb359b6a3b
Pradeep Chandu <58159402+oreodigi@users.noreply.github.com>
2026-08-28 18:02:24 +0530

    Link business moderation to social publishing

--- app/admin/reviews/businesses/page.tsx
+++ app/admin/reviews/businesses/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+import {Share2} from "lucide-react";
 import BusinessReviewWorkspace from "@/components/BusinessReviewWorkspace";
 import "../review.css";
-export default function BusinessReviewsPage(){return <BusinessReviewWorkspace/>}
+export default function BusinessReviewsPage(){return <><div className="review-social-shortcut"><Link href="/admin/social"><Share2 size={15}/>Promote approved businesses</Link></div><BusinessReviewWorkspace/></>}
