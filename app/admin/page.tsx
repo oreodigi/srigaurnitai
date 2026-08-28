978be4688d3629dd9e99aadafda7f872a94833c4
Pradeep Chandu <58159402+oreodigi@users.noreply.github.com>
2026-08-28 16:09:49 +0530

    Replace admin overview with analytics dashboard

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,8 +1,10 @@
 import "./admin-master.css";
+import "./dashboard-v2.css";
 import AdminMasterConsole from "./AdminMasterConsole";
 import AdminSeoEnhancer from "./AdminSeoEnhancer";
 import AdminExperienceEnhancer from "./AdminExperienceEnhancer";
+import AdminDashboardV2 from "./AdminDashboardV2";
 
 export default function AdminPage(){
-  return <><AdminMasterConsole/><AdminSeoEnhancer/><AdminExperienceEnhancer/></>;
+  return <><AdminMasterConsole/><AdminSeoEnhancer/><AdminExperienceEnhancer/><AdminDashboardV2/></>;
 }
