fbc11b8041a115ba8380c99cb72b7c4c9a02f90d
Pradeep Chandu <58159402+oreodigi@users.noreply.github.com>
2026-08-28 09:53:30 +0530

    Remove legacy bottom admin utility bar

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,6 @@
 import "./admin-master.css";
 import AdminMasterConsole from "./AdminMasterConsole";
-import { AdminUtilityLinks } from "./AdminUtilityLinks";
 
 export default function AdminPage(){
-  return <><AdminMasterConsole/><AdminUtilityLinks/></>;
+  return <AdminMasterConsole/>;
 }
